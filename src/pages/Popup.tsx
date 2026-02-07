8cf4b130f0c8760e6ba54cc1169847237de0208a
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2026-02-07 20:40:14 +0100

    refactor: rename interval hook

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -1,14 +1,14 @@
 import Heading from '../components/Heading';
 import Stats from '../components/Stats';
 import Action from '../components/Action';
-import useInterval from '../hooks/useInterval';
+import useBreakInterval from '../hooks/useBreakInterval';
 import useIsTimerActive from '../hooks/useIsTimerActive';
 import Settings from '../components/Settings';
 import Layout from '../layouts/Layout';
 
 
 export default function Popup() {
-  const { interval, handleChangeInterval } = useInterval()
+  const { interval, handleChangeInterval } = useBreakInterval()
   const { isActive, handleToggleActive } = useIsTimerActive()
   return (
     <Layout>
