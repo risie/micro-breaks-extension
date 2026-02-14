85eada30f35157c5fd06651ca442f4f74075d3a7
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2026-02-14 22:57:01 +0100

    chore: leave layouts out for now
    will have to track down the root cause later

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from "react"
 
-// interface Props {
-//   children: ReactNode
-// }
-export function Layout({ children }: any) {
+interface Props {
+  children: ReactNode
+}
+export function Layout({ children }: Props) {
   return (
     <div className='card bg-base-100 w-96 h-full gap-4 shadow-sm m-4 flex flex-col justify-between'>
       {children}
