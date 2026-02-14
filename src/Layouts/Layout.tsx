8817f82d28f2495b0898fa833bedc94c19cd1025
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2026-02-14 22:53:00 +0100

    chore: test without react

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,9 +1,8 @@
-import { ReactNode } from "react"
 
-interface Props {
-  children: ReactNode
-}
-export function Layout({ children }: Props) {
+// interface Props {
+//   children: ReactNode
+// }
+export function Layout({ children }: any) {
   return (
     <div className='card bg-base-100 w-96 h-full gap-4 shadow-sm m-4 flex flex-col justify-between'>
       {children}
--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -4,18 +4,19 @@ import { Settings } from "../components/Settings";
 import { Stats } from "../components/Stats";
 import { useBreakInterval } from "../hooks/useBreakInterval";
 import { useIsTimerActive } from "../hooks/useIsTimerActive";
+import { Layout } from "../layouts/Layout";
 
 
 export default function Popup() {
   const { interval, handleChangeInterval } = useBreakInterval()
   const { isActive, handleToggleActive } = useIsTimerActive()
   return (
-    <>
+    <Layout>
       <Heading />
       <Settings interval={interval} onChange={handleChangeInterval} />
       <Stats />
       <Action active={isActive} onClick={handleToggleActive} />
-    </>
+    </Layout>
 
   );
 }
